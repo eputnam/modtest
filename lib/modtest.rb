1cd01a3e913136c69e51022cbe4bda3acf38d648
Eric Putnam <putnam.eric@gmail.com>
2016-12-14 19:27:54 -0800

    finished

--- lib/modtest.rb
+++ lib/modtest.rb
@@ -47,61 +47,59 @@ class Modtest
     command :acceptance do |c|
       c.syntax = 'modtest acceptance [options]'
       c.description = "Runs acceptance tests"
-      c.option '--options', Object, 'list selected options and do nothing'
-      c.option '--install-type INSTALL_TYPE', String, 'install type'
-      c.option '--node NODESET', String, 'path to nodeset for the test(s)'
-      c.option '--key KEYFILE', String, 'path to key file for acceptance tests'
-      c.option '--version VERSION', Numeric, 'puppet version'
-      c.option '-d','--destroy', String, 'to destroy or not to destroy?'
-      c.option '--provision', Object, 'to provision or not to provision?'
-      c.option '-D','--debug', Object, 'debug?'
-      c.option '--test FILE', String, 'Test file'
+      c.option '-o', '--options', Object, 'list selected options and do nothing'
+      c.option '-e', '--type TYPE', String, 'install type'
+      c.option '-n', '--node NODESET', String, 'path to nodeset for the test(s)'
+      c.option '-k', '--key KEYFILE', String, 'path to key file for acceptance tests'
+      c.option '-i', '--install-version INSTALL_VERSION', Numeric, 'puppet version'
+      c.option '-d', '--destroy', String, 'to destroy or not to destroy?'
+      c.option '-p', '--provision', Object, 'to provision or not to provision?'
+      c.option '-b', '--debug', Object, 'debug?'
+      c.option '-f', '--file FILE', String, 'Test file'
       c.action do |args,opts|
 
-        case opts.install_type.downcase
+        opts.default \
+        :key       => "#{ENV['HOME']}/.ssh/id_rsa-acceptance",
+        :destroy   => ENV['BEAKER_destroy'] || false,
+        :provision => ENV['BEAKER_provision'] || false,
+        :type      => "foss"
+
+        case opts.type.downcase
         when "pe"
           puppet_install_type = "pe"
         when "foss"
           puppet_install_type = "foss"
         when "agent"
           puppet_install_type = "agent"
-        else
-          puppet_install_type = "foss"
         end
 
-        if opts.install_type.downcase == "pe"
-          if opts.version
-            pe_version_actual = `curl -s http://getpe.delivery.puppetlabs.net/latest/#{opts.version}`
-            pe_dir = "http://enterprise.delivery.puppetlabs.net/#{opts.version}/ci-ready"
+        if puppet_install_type == "pe"
+          if opts.install_version
+            pe_version_actual = `curl -s http://getpe.delivery.puppetlabs.net/latest/#{opts.install_version}`
+            pe_dir = "http://enterprise.delivery.puppetlabs.net/#{opts.install_version}/ci-ready"
           else
             raise "PE requires a version number. \nExamples: 2016.4, 2016.2, 2015.3\n"
           end
-        elsif opts.install_type.downcase == "foss"
-          if opts.version
-            puppet_install_version = version
-          else
-            print "Using latest FOSS version...\n"
+        elsif puppet_install_type == "foss"
+          if opts.install_version
+            puppet_install_version = opts.install_version
           end
-        elsif opts.install_type.downcase == "agent"
-          if opts.version
-            puppet_install_version = version
+        elsif puppet_install_type == "agent"
+          if opts.install_version
+            puppet_install_version = opts.install_version
           end
         end
 
-        opts.default \
-        :key => "#{ENV['HOME']}/.ssh/id_rsa-acceptance",
-        :destroy => ENV['BEAKER_destroy'] || false,
-        :provision => ENV['BEAKER_provision'] || false
-
         @final_command_hash = {
-          "PUPPET_INSTALL_TYPE" => puppet_install_type,
-          "BEAKER_PE_DIR"       => pe_dir,
-          "BEAKER_PE_VER"       => pe_version_actual,
-          "BEAKER_destroy"      => tf_to_yn(options.destroy),
-          "BEAKER_provision"    => tf_to_yn(options.provision),
-          "BEAKER_setfile"      => options.node,
-          "BEAKER_keyfile"      => options.key,
-          "BEAKER_debug"        => options.debug
+          "PUPPET_INSTALL_TYPE"    => puppet_install_type,
+          "PUPPET_INSTALL_VERSION" => puppet_install_version,
+          "BEAKER_PE_DIR"          => pe_dir,
+          "BEAKER_PE_VER"          => pe_version_actual,
+          "BEAKER_destroy"         => tf_to_yn(opts.destroy),
+          "BEAKER_provision"       => tf_to_yn(opts.provision),
+          "BEAKER_setfile"         => opts.node,
+          "BEAKER_keyfile"         => opts.key,
+          "BEAKER_debug"           => opts.debug
         }.delete_if { |key, value| value.nil? }
 
         @final_command_hash.each do |key,value|
@@ -116,13 +114,13 @@ class Modtest
           raise "Acceptance test directory: #{Dir.pwd}/spec/acceptance not found, are you in the module's root dir?"
         end
 
-        rspec_command = "bundle exec rspec #{Dir.pwd}/spec/acceptance/#{options.test}"
+        rspec_command = "bundle exec rspec #{Dir.pwd}/spec/acceptance/#{opts.test}"
         @final_command_string += rspec_command
 
         print_options @final_command_hash
         print_command @final_command_hash, rspec_command
 
-        unless options.options
+        unless opts.options
           exec(@final_command_string)
         end
       end
